6efe172d2c4fdd625ce1e402bf826de3df0bb61f
Henry Fong <fong03@gmail.com>
2021-04-15 14:44:16 -0400

    change html template

--- baseballPred/urls.py
+++ baseballPred/urls.py
@@ -1,4 +1,6 @@
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 app_name = 'baseballPred'
@@ -6,4 +8,4 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('data/', views.data, name='data'),
     path('ajax/data/teams/', views.ajax_get_team_winrates, name='ajax_get_team_winrates')
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
